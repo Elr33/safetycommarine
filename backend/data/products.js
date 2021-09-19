52565f595e1de6aff32243f2e31db9834eb22d69
Etienne le Roux <efleroux@gmail.com>
2021-09-19 20:39:04 +0200

    added all pictures, some still to remove transparency

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,16 +1,5 @@
 const products = [
-  {
-    name: 'Professional Overhaul pants',
-    image: '/images/pants.png',
-    description:
-      'Fashionable and Trendy, Avon range of top notch handbags will be the next nest accessory that you can acquire for a fraction of the price of buying it in Store',
-    brand: 'Avon',
-    category: 'Fragrance',
-    price: 1200,
-    countInStock: 20,
-    rating: 0,
-    numReviews: 0,
-  },
+  
   {
     name: 'Motorola Car Scanner ',
     image: '/images/moto.png',
@@ -31,6 +20,42 @@ const products = [
     brand: 'Baofang',
     category: 'two way radio',
     price: 1850,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Swage Dome',
+    image: '/images/316s-stainless-steel-swage-dome-terminal-end-wire-rope-balustrade-fittings-marine-hardware-removebg-preview.png',
+    description:
+      'Dome Terminal End For 3mm and 4mm SS Wire Rope, Used For Balustrading.',
+    brand: '316SS',
+    category: 'Stainless Steel Hardware',
+    price: 5,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pelican Hook',
+    image: '/images/316ss-pelican-hook-quick-release-clip-marine-hardware-stainless-steel-fittings-boat-safety-clips-removebg-preview.png',
+    description:
+      'Pelican Hook or Senhouse Slip Hook for securing life rafts in 316 Stainless Steel',
+    brand: '316SS',
+    category: 'Stainless Steel Hardware',
+    price: 8,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pelican Hook',
+    image: '/images/316ss-samson-post-anchor-bollard-boat-deck-fittings-removebg-preview.png',
+    description:
+      '316 Stainless Steel Samson Post available in the following size Small BG80A Plate size 60mm x 60mm Height - 69mm Large BG80 Plate 87mm x 76mm, Height - 77mm',
+    brand: '316SS',
+    category: 'Stainless Steel Hardware',
+    price: 35,
     countInStock: 20,
     rating: 0,
     numReviews: 0,
